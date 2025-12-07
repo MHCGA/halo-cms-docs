0d73c465b6fbe1f3df88041301d801481d12bfc2
Howie Xie <94725606+HowieHz@users.noreply.github.com>
2025-12-07 18:45:34 +0800

    feat: Add enhanced readability plugin to VitePress docs (#34)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -219,6 +219,16 @@ export default defineConfig({
         },
       } satisfies RSSOptions),
     ],
+    optimizeDeps: {
+      exclude: ["@nolebase/vitepress-plugin-enhanced-readabilities/client", "vitepress", "@nolebase/ui"],
+    },
+    ssr: {
+      noExternal: [
+        "@nolebase/vitepress-plugin-enhanced-readabilities",
+        "@nolebase/ui",
+        "@nolebase/vitepress-plugin-highlight-targeted-heading",
+      ],
+    },
   },
   base: basePath,
 
