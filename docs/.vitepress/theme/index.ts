06de44a7e53c854eb0f71efd04a0f433c28f52ec
Howie Xie <94725606+HowieHz@users.noreply.github.com>
2025-12-09 19:16:17 +0800

    chore: Remove enhanced readabilities plugin from VitePress docs (#42)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import {
-  NolebaseEnhancedReadabilitiesMenu,
-  NolebaseEnhancedReadabilitiesScreenMenu,
-} from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { h } from "vue";
@@ -11,7 +7,6 @@ import GiscusComment from "./components/GiscusComment.vue";
 import PageFooterNotice from "./components/PageFooterNotice.vue";
 import PostMetadata from "./components/PostMetadata.vue";
 
-import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
 import "./style.css";
 
 import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading/client";
@@ -30,10 +25,6 @@ export default {
           category: "Announcements",
           categoryId: "DIC_kwDOQgjPtc4CzQs-",
         }),
-      // 为较宽的屏幕的导航栏添加阅读增强菜单
-      "nav-bar-content-after": () => [h(NolebaseEnhancedReadabilitiesMenu)],
-      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
-      "nav-screen-content-after": () => [h(NolebaseEnhancedReadabilitiesScreenMenu)],
       // 闪烁高亮当前的目标标题
       "layout-top": () => [h(NolebaseHighlightTargetedHeading)],
     });
