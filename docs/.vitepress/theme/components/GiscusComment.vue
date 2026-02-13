2c468c1218abbabf138b354c199cb2061744b32a
HowieHz <howie_hzgo@outlook.com>
2026-02-13 18:17:37 +0800

    docs: Cleanup Vue templates and format docs

--- docs/.vitepress/theme/components/GiscusComment.vue
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-if="shouldShow"
-    class="giscus-wrapper"
-  >
+  <div v-if="shouldShow" class="giscus-wrapper">
     <Giscus
       :repo="repo"
       :repo-id="repoId"
