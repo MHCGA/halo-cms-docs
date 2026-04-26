f00210bc8424af180f1530a9ef899cf72a149746
HowieHz <94725606+HowieHz@users.noreply.github.com>
2026-04-26 18:35:16 +0000

    style: auto-format and fixes

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,4 @@
-/** @type {import('stylelint').Config} */
+/** @type {import("stylelint").Config} */
 export default {
   extends: ["stylelint-config-standard", "stylelint-config-html", "stylelint-config-recommended-vue"],
   plugins: ["stylelint-order"],
